4e018011b5a552197b608bf1072443094770880a
fifovalle <fifanaufal10@gmail.com>
2024-07-21 13:28:05 +0700

    Tinggal router lupa sandi

--- app/(halamanautentikasi)/daftar.tsx
+++ app/(halamanautentikasi)/daftar.tsx
@@ -5,149 +5,217 @@ import {
   TouchableOpacity,
   Pressable,
   Image,
+  ScrollView,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Link } from "expo-router";
 import React, { useState } from "react";
+import {
+  useFonts,
+  Poppins_700Bold,
+  Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
 
 export default function HalamanDaftar() {
   const [email, aturEmail] = useState("");
   const [kataSandi, aturKataSandi] = useState("");
+  const [konfirmasiKataSandi, aturKonfirmasiKataSandi] = useState("");
   const [ceklis, aturCeklis] = useState(false);
   const [lihatkanKataSandi, aturLihatkanKataSandi] = useState(false);
+  const [lihatkanKonfirmasiKataSandi, aturLihatkanKonfirmasiKataSandi] =
+    useState(false);
   const Mata = require("../../assets/images/Ikon2.png");
   const Mata2 = require("../../assets/images/Ikon5.png");
   const Google = require("../../assets/images/Ikon3.png");
   const Gambar1 = require("../../assets/images/Kepala.png");
+  let [memuatFont] = useFonts({
+    Poppins_700Bold,
+    Poppins_600SemiBold,
+  });
+  if (!memuatFont) {
+    return null;
+  }
 
   return (
-    <View className="flex-1">
-      <View className="w-full h-80">
-        <Image source={Gambar1} className="object-cover w-full h-full" />
-      </View>
-      <View
-        className="w-[330px] h-[440px] mx-auto rounded-2xl mt-3 bg-white"
-        style={{
-          shadowColor: "#000",
-          shadowOffset: {
-            width: 0,
-            height: 8,
-          },
-          shadowOpacity: 0.44,
-          shadowRadius: 10.32,
-          elevation: 16,
-        }}
-      >
-        <View className="flex-row mt-10 justify-evenly w-80 mx-auto bg-white">
-          <TouchableOpacity
-            className="bg-white px-8 py-3 rounded-full z-10 shadow-md w-[11rem] ml-4"
-            activeOpacity={0.7}
-          >
-            <Link
-              className="text-[#275229] font-bold text-center"
-              href={"/"}
-              replace={true}
-            >
-              <Text>Bergabung</Text>
-            </Link>
-          </TouchableOpacity>
-          <TouchableOpacity
-            className="bg-[#275229] px-8 py-3 rounded-r-full shadow-md border border-l-0 border-[#275229] w-[11rem] mr-4"
-            activeOpacity={0.7}
-          >
-            <Link
-              className="text-white font-bold text-center"
-              href={"/daftar"}
-              replace={true}
-            >
-              <Text>Mendaftar</Text>
-            </Link>
-          </TouchableOpacity>
+    <ScrollView className="flex-1" keyboardShouldPersistTaps="handled">
+      <View className="flex-1">
+        <View className="w-full h-80">
+          <Image source={Gambar1} className="object-cover w-full h-full" />
         </View>
-        <View className="mt-10 px-8">
-          <View className="relative">
-            <TextInput
-              className="border-gray-300 border-b-2 py-2 focus:outline-none focus:border-[#275229]"
-              onChangeText={aturEmail}
-              value={email}
-            />
-            <Text
-              className={`absolute top-0 left-0 text-gray-500 transition-all ${
-                email ? "text-sm -top-6" : "text-base top-2"
-              }`}
-              style={{
-                translateY: email ? -6 : 2,
-                opacity: email ? 0.5 : 1,
-              }}
+        <View
+          className="w-[330px] h-[440px] mx-auto rounded-2xl mt-3 bg-white"
+          style={{
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 8,
+            },
+            shadowOpacity: 0.44,
+            shadowRadius: 10.32,
+            elevation: 16,
+          }}
+        >
+          <View className="flex-row mt-8 justify-evenly w-80 mx-auto bg-white">
+            <TouchableOpacity
+              className="bg-white px-8 py-3 rounded-full z-10 shadow-md w-[11rem] ml-4"
+              activeOpacity={0.7}
             >
-              Masukan email anda
-            </Text>
-          </View>
-          <View className="relative mt-4">
-            <TextInput
-              className="border-gray-300 border-b-2 py-2 focus:outline-none focus:border-[#275229]"
-              secureTextEntry={!lihatkanKataSandi}
-              onChangeText={aturKataSandi}
-              value={kataSandi}
-            />
-            <Text
-              className={`absolute top-0 left-0 text-gray-500 transition-all ${
-                kataSandi ? "text-sm -top-6" : "text-base top-2"
-              }`}
-              style={{
-                translateY: kataSandi ? -6 : 0,
-                opacity: kataSandi ? 0.5 : 1,
-              }}
-            >
-              Masukan Kata Sandi Anda
-            </Text>
-            <Pressable
-              onPress={() => aturLihatkanKataSandi(!lihatkanKataSandi)}
-              className="absolute top-4 right-4"
-            >
-              <Image
-                source={lihatkanKataSandi ? Mata2 : Mata}
-                className="w-7 h-7"
-              />
-            </Pressable>
-          </View>
-          <View className="flex-row mt-4 justify-between">
-            <View className="flex-row items-center">
-              <Pressable
-                className={`w-5 h-5 justify-center items-center rounded ${
-                  ceklis ? "bg-[#275229]" : "border-gray-300 border-2"
-                }`}
-                onPress={() => aturCeklis(!ceklis)}
+              <Link
+                className="text-[#275229] text-center text-lg"
+                href={"/"}
+                replace={true}
               >
-                {ceklis && (
-                  <Ionicons name="checkmark" size={12} color="white" />
-                )}
-              </Pressable>
-              <Text className="text-center ml-1">Ingat Saya</Text>
-            </View>
-            <TouchableOpacity activeOpacity={0.7}>
-              <Text className="text-gray-500">Lupa Sandi?</Text>
+                <Text style={{ fontFamily: "Poppins_700Bold" }}>Bergabung</Text>
+              </Link>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="bg-[#275229] px-8 py-3 rounded-r-full shadow-md border border-l-0 border-[#275229] w-[11rem] mr-4"
+              activeOpacity={0.7}
+            >
+              <Link
+                className="text-white font-bold text-center text-lg"
+                href={"/daftar"}
+                replace={true}
+              >
+                <Text style={{ fontFamily: "Poppins_700Bold" }}>Mendaftar</Text>
+              </Link>
             </TouchableOpacity>
           </View>
-          <TouchableOpacity
-            className="bg-[#275229] py-3 rounded-xl mt-8 mx-8 shadow-md"
-            activeOpacity={0.7}
-          >
-            <Text className="text-center text-white font-bold">Bergabung</Text>
-          </TouchableOpacity>
-          <View className="flex-row justify-center mt-8">
-            <Text className="text-gray-500">--ATAU--</Text>
-          </View>
-          <TouchableOpacity
-            className="flex-row justify-center mt-4"
-            activeOpacity={0.7}
-          >
-            <View className="bg-white border rounded-full p-2 shadow-md">
-              <Image source={Google} className="w-6 h-6" />
+          <View className="mt-5 px-8">
+            <View className="relative">
+              <TextInput
+                className="border-gray-300 border-b-2 py-2 focus:outline-none focus:border-[#275229]"
+                onChangeText={aturEmail}
+                value={email}
+              />
+              <Text
+                className={`absolute top-0 left-0 text-gray-500 transition-all ${
+                  email ? "text-sm -top-6" : "text-base top-2"
+                }`}
+                style={{
+                  translateY: email ? -6 : 2,
+                  opacity: email ? 0.5 : 1,
+                }}
+              >
+                E-mail
+              </Text>
             </View>
-          </TouchableOpacity>
+            <View className="relative mt-4">
+              <TextInput
+                className="border-gray-300 border-b-2 py-2 focus:outline-none focus:border-[#275229]"
+                secureTextEntry={!lihatkanKataSandi}
+                onChangeText={aturKataSandi}
+                value={kataSandi}
+              />
+              <Text
+                className={`absolute top-0 left-0 text-gray-500 transition-all ${
+                  kataSandi ? "text-sm -top-6" : "text-base top-2"
+                }`}
+                style={{
+                  translateY: kataSandi ? -6 : 0,
+                  opacity: kataSandi ? 0.5 : 1,
+                }}
+              >
+                Kata Sandi
+              </Text>
+              <Pressable
+                onPress={() => aturLihatkanKataSandi(!lihatkanKataSandi)}
+                className="absolute top-4 right-4"
+              >
+                <Image
+                  source={lihatkanKataSandi ? Mata2 : Mata}
+                  className="w-7 h-7"
+                />
+              </Pressable>
+            </View>
+            <View className="relative mt-4">
+              <TextInput
+                className="border-gray-300 border-b-2 py-2 focus:outline-none focus:border-[#275229]"
+                secureTextEntry={!lihatkanKonfirmasiKataSandi}
+                onChangeText={aturKonfirmasiKataSandi}
+                value={konfirmasiKataSandi}
+              />
+              <Text
+                className={`absolute top-0 left-0 text-gray-500 transition-all ${
+                  konfirmasiKataSandi ? "text-sm -top-6" : "text-base top-2"
+                }`}
+                style={{
+                  translateY: konfirmasiKataSandi ? -6 : 0,
+                  opacity: konfirmasiKataSandi ? 0.5 : 1,
+                }}
+              >
+                Konfirmasi Kata Sandi
+              </Text>
+              <Pressable
+                onPress={() =>
+                  aturLihatkanKonfirmasiKataSandi(!lihatkanKonfirmasiKataSandi)
+                }
+                className="absolute top-4 right-4"
+              >
+                <Image
+                  source={lihatkanKonfirmasiKataSandi ? Mata2 : Mata}
+                  className="w-7 h-7"
+                />
+              </Pressable>
+            </View>
+            <View className="flex-row mt-4 justify-between">
+              <View className="flex-row items-center">
+                <Pressable
+                  className={`w-5 h-5 justify-center items-center rounded ${
+                    ceklis ? "bg-[#275229]" : "border-gray-300 border-2"
+                  }`}
+                  onPress={() => aturCeklis(!ceklis)}
+                >
+                  {ceklis && (
+                    <Ionicons name="checkmark" size={12} color="white" />
+                  )}
+                </Pressable>
+                <Text
+                  className="text-center ml-1 text-gray-500"
+                  style={{ fontFamily: "Poppins_600SemiBold" }}
+                >
+                  Ingat Saya
+                </Text>
+              </View>
+              <TouchableOpacity activeOpacity={0.7}>
+                <Text
+                  className="text-gray-500"
+                  style={{ fontFamily: "Poppins_600SemiBold" }}
+                >
+                  Lupa Sandi?
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity
+              className="bg-[#275229] py-3 rounded-xl mt-4 mx-8 shadow-md"
+              activeOpacity={0.7}
+            >
+              <Text
+                className="text-center text-white font-bold text-xl"
+                style={{ fontFamily: "Poppins_700Bold" }}
+              >
+                Bergabung
+              </Text>
+            </TouchableOpacity>
+            <View className="flex-row justify-center mt-3">
+              <Text
+                className="text-gray-500"
+                style={{ fontFamily: "Poppins_600SemiBold" }}
+              >
+                --ATAU--
+              </Text>
+            </View>
+            <TouchableOpacity
+              className="flex-row justify-center mt-1"
+              activeOpacity={0.7}
+            >
+              <View className="bg-white border rounded-full p-2 shadow-md">
+                <Image source={Google} className="w-6 h-6" />
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
--- app/(halamanautentikasi)/index.tsx
+++ app/(halamanautentikasi)/index.tsx
@@ -5,10 +5,16 @@ import {
   TouchableOpacity,
   Pressable,
   Image,
+  ScrollView,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Link } from "expo-router";
 import React, { useState } from "react";
+import {
+  useFonts,
+  Poppins_700Bold,
+  Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
 
 export default function HalamanGabung() {
   const [email, aturEmail] = useState("");
@@ -19,135 +25,165 @@ export default function HalamanGabung() {
   const Mata2 = require("../../assets/images/Ikon5.png");
   const Google = require("../../assets/images/Ikon3.png");
   const Gambar1 = require("../../assets/images/Kepala.png");
+  let [memuatFont] = useFonts({
+    Poppins_700Bold,
+    Poppins_600SemiBold,
+  });
+  if (!memuatFont) {
+    return null;
+  }
 
   return (
-    <View className="flex-1">
-      <View className="w-full h-80">
-        <Image source={Gambar1} className="object-cover w-full h-full" />
-      </View>
-      <View
-        className="w-[330px] h-[440px] mx-auto rounded-2xl mt-3 bg-white"
-        style={{
-          shadowColor: "#000",
-          shadowOffset: {
-            width: 0,
-            height: 8,
-          },
-          shadowOpacity: 0.44,
-          shadowRadius: 10.32,
-          elevation: 16,
-        }}
-      >
-        <View className="flex-row mt-10 justify-evenly w-80 mx-auto bg-white">
-          <TouchableOpacity
-            className="bg-[#275229] px-8 py-3 rounded-full z-10 shadow-md w-[11rem] ml-4"
-            activeOpacity={0.7}
-          >
-            <Link
-              href={"/"}
-              className="text-white font-bold text-center"
-              replace={true}
-            >
-              <Text>Bergabung</Text>
-            </Link>
-          </TouchableOpacity>
-          <TouchableOpacity
-            className="bg-white px-8 py-3 rounded-r-full shadow-md border border-l-0 border-[#275229] w-[11rem] mr-4"
-            activeOpacity={0.7}
-          >
-            <Link
-              className="text-[#275229] font-bold text-center"
-              href={"/daftar"}
-              replace={true}
-            >
-              <Text>Mendaftar</Text>
-            </Link>
-          </TouchableOpacity>
+    <ScrollView className="flex-1" keyboardShouldPersistTaps="handled">
+      <View className="flex-1">
+        <View className="w-full h-80">
+          <Image source={Gambar1} className="object-cover w-full h-full" />
         </View>
-        <View className="mt-10 px-8">
-          <View className="relative">
-            <TextInput
-              className="border-gray-300 border-b-2 py-2 focus:outline-none focus:border-[#275229]"
-              onChangeText={aturEmail}
-              value={email}
-            />
-            <Text
-              className={`absolute top-0 left-0 text-gray-500 transition-all ${
-                email ? "text-sm -top-6" : "text-base top-2"
-              }`}
-              style={{
-                translateY: email ? -6 : 2,
-                opacity: email ? 0.5 : 1,
-              }}
+        <View
+          className="w-[330px] h-[440px] mx-auto rounded-2xl mt-3 bg-white"
+          style={{
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 8,
+            },
+            shadowOpacity: 0.44,
+            shadowRadius: 10.32,
+            elevation: 16,
+          }}
+        >
+          <View className="flex-row mt-10 justify-evenly w-80 mx-auto bg-white">
+            <TouchableOpacity
+              className="bg-[#275229] px-8 py-3 rounded-full z-10 shadow-md w-[11rem] ml-4"
+              activeOpacity={0.7}
             >
-              Masukan email anda
-            </Text>
-          </View>
-          <View className="relative mt-4">
-            <TextInput
-              className="border-gray-300 border-b-2 py-2 focus:outline-none focus:border-[#275229]"
-              secureTextEntry={!lihatkanKataSandi}
-              onChangeText={aturKataSandi}
-              value={kataSandi}
-            />
-            <Text
-              className={`absolute top-0 left-0 text-gray-500 transition-all ${
-                kataSandi ? "text-sm -top-6" : "text-base top-2"
-              }`}
-              style={{
-                translateY: kataSandi ? -6 : 0,
-                opacity: kataSandi ? 0.5 : 1,
-              }}
-            >
-              Masukan Kata Sandi Anda
-            </Text>
-            <Pressable
-              onPress={() => aturLihatkanKataSandi(!lihatkanKataSandi)}
-              className="absolute top-4 right-4"
-            >
-              <Image
-                source={lihatkanKataSandi ? Mata2 : Mata}
-                className="w-7 h-7"
-              />
-            </Pressable>
-          </View>
-          <View className="flex-row mt-4 justify-between">
-            <View className="flex-row items-center">
-              <Pressable
-                className={`w-5 h-5 justify-center items-center rounded ${
-                  ceklis ? "bg-[#275229]" : "border-gray-300 border-2"
-                }`}
-                onPress={() => aturCeklis(!ceklis)}
+              <Link
+                href={"/"}
+                className="text-white text-center text-lg"
+                replace={true}
               >
-                {ceklis && (
-                  <Ionicons name="checkmark" size={12} color="white" />
-                )}
-              </Pressable>
-              <Text className="text-center ml-1">Ingat Saya</Text>
-            </View>
-            <TouchableOpacity activeOpacity={0.7}>
-              <Text className="text-gray-500">Lupa Sandi?</Text>
+                <Text style={{ fontFamily: "Poppins_700Bold" }}>Bergabung</Text>
+              </Link>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="bg-white px-8 py-3 rounded-r-full shadow-md border border-l-0 border-[#275229] w-[11rem] mr-4"
+              activeOpacity={0.7}
+            >
+              <Link
+                className="text-[#275229] text-center text-lg"
+                href={"/daftar"}
+                replace={true}
+              >
+                <Text style={{ fontFamily: "Poppins_700Bold" }}>Mendaftar</Text>
+              </Link>
             </TouchableOpacity>
           </View>
-          <TouchableOpacity
-            className="bg-[#275229] py-3 rounded-xl mt-8 mx-8 shadow-md"
-            activeOpacity={0.7}
-          >
-            <Text className="text-center text-white font-bold">Bergabung</Text>
-          </TouchableOpacity>
-          <View className="flex-row justify-center mt-8">
-            <Text className="text-gray-500">--ATAU--</Text>
-          </View>
-          <TouchableOpacity
-            className="flex-row justify-center mt-4"
-            activeOpacity={0.7}
-          >
-            <View className="bg-white border rounded-full p-2 shadow-md">
-              <Image source={Google} className="w-6 h-6" />
+          <View className="mt-10 px-8">
+            <View className="relative">
+              <TextInput
+                className="border-gray-300 border-b-2 py-2 focus:outline-none focus:border-[#275229]"
+                onChangeText={aturEmail}
+                value={email}
+              />
+              <Text
+                className={`absolute top-0 left-0 text-gray-500 transition-all ${
+                  email ? "text-sm -top-6" : "text-base top-2"
+                }`}
+                style={{
+                  translateY: email ? -6 : 2,
+                  opacity: email ? 0.5 : 1,
+                }}
+              >
+                E-mail
+              </Text>
             </View>
-          </TouchableOpacity>
+            <View className="relative mt-4">
+              <TextInput
+                className="border-gray-300 border-b-2 py-2 focus:outline-none focus:border-[#275229]"
+                secureTextEntry={!lihatkanKataSandi}
+                onChangeText={aturKataSandi}
+                value={kataSandi}
+              />
+              <Text
+                className={`absolute top-0 left-0 text-gray-500 transition-all ${
+                  kataSandi ? "text-sm -top-6" : "text-base top-2"
+                }`}
+                style={{
+                  translateY: kataSandi ? -6 : 0,
+                  opacity: kataSandi ? 0.5 : 1,
+                }}
+              >
+                Kata Sandi
+              </Text>
+              <Pressable
+                onPress={() => aturLihatkanKataSandi(!lihatkanKataSandi)}
+                className="absolute top-4 right-4"
+              >
+                <Image
+                  source={lihatkanKataSandi ? Mata2 : Mata}
+                  className="w-7 h-7"
+                />
+              </Pressable>
+            </View>
+            <View className="flex-row mt-4 justify-between">
+              <View className="flex-row items-center">
+                <Pressable
+                  className={`w-5 h-5 justify-center items-center rounded ${
+                    ceklis ? "bg-[#275229]" : "border-gray-300 border-2"
+                  }`}
+                  onPress={() => aturCeklis(!ceklis)}
+                >
+                  {ceklis && (
+                    <Ionicons name="checkmark" size={12} color="white" />
+                  )}
+                </Pressable>
+                <Text
+                  className="ml-1 text-gray-500"
+                  style={{ fontFamily: "Poppins_600SemiBold" }}
+                >
+                  Ingat Saya
+                </Text>
+              </View>
+              <TouchableOpacity activeOpacity={0.7}>
+                <Text
+                  className="text-gray-500"
+                  style={{ fontFamily: "Poppins_600SemiBold" }}
+                >
+                  Lupa Sandi?
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity
+              className="bg-[#275229] py-3 rounded-xl mt-8 mx-8 shadow-md"
+              activeOpacity={0.7}
+            >
+              <Text
+                className="text-center text-white font-bold text-xl"
+                style={{ fontFamily: "Poppins_700Bold" }}
+              >
+                Bergabung
+              </Text>
+            </TouchableOpacity>
+            <View className="flex-row justify-center mt-6">
+              <Text
+                className="text-gray-500"
+                style={{ fontFamily: "Poppins_600SemiBold" }}
+              >
+                --ATAU--
+              </Text>
+            </View>
+            <TouchableOpacity
+              className="flex-row justify-center mt-2"
+              activeOpacity={0.7}
+            >
+              <View className="bg-white border rounded-full p-2 shadow-md">
+                <Image source={Google} className="w-6 h-6" />
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
