69ab65478ebd5f44185acb164487cc2b8712fe4f
fifovalle <fifanaufal10@gmail.com>
2024-07-21 08:37:19 +0700

    Memodifikasi file components

--- app/components/KepalaAutentikasi.tsx
+++ app/components/KepalaAutentikasi.tsx
@@ -2,13 +2,11 @@ import { View, Image } from "react-native";
 import React from "react";
 
 export default function KepalaAutentikasi() {
+  const Gambar1 = require("../../assets/images/Kepala.png");
   return (
     <View>
       <View className="w-full h-80">
-        <Image
-          source={require("../../assets/images/Kepala.png")}
-          className="object-cover w-full h-full"
-        />
+        <Image source={Gambar1} className="object-cover w-full h-full" />
       </View>
     </View>
   );
