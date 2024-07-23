1b1c59b972b85c6b49e68285f4c3a594735c19da
fifovalle <fifanaufal10@gmail.com>
2024-07-23 09:02:18 +0700

    Beranda baru bar pencaharian dlu

---
+++ app/(halamanutama)/_layout.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "expo-router";
+
+export default function TataLetakUtama() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(halamanautentikasi)" />
+      {/* <Stack.Screen name="(halamanautentikasi)" /> */}
+      <Stack.Screen name="(halamanutama)" />
     </Stack>
   );
 }
